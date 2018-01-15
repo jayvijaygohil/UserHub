1617b164c07f64ea77fa9897fae626655420612a
jayvijaygohil <jayvijaygohildev@outlook.com>
2018-01-14 20:08:30 -0500

    Complete adapter implementation

--- app/src/main/java/com/jvgohil/userhub/home/RepoListAdapter.java
+++ app/src/main/java/com/jvgohil/userhub/home/RepoListAdapter.java
@@ -2,6 +2,7 @@ package com.jvgohil.userhub.home;
 
 import android.arch.lifecycle.LifecycleOwner;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -31,22 +32,28 @@ public class RepoListAdapter extends RecyclerView.Adapter<RepoListAdapter.RepoVi
                 notifyDataSetChanged(); //TODO: Implement DiffUtils after learning AutoValue
             }
         });
-
+        setHasStableIds(true);
     }
 
     @Override
     public RepoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_repo_list, parent, false);
+        return new RepoViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(RepoViewHolder holder, int position) {
-
+        holder.bind(data.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return data.get(position).id;
     }
 
     static final class RepoViewHolder extends RecyclerView.ViewHolder {
@@ -56,7 +63,7 @@ public class RepoListAdapter extends RecyclerView.Adapter<RepoListAdapter.RepoVi
         @BindView(R.id.tv_forks) TextView repoForks;
         @BindView(R.id.tv_stars) TextView repoStars;
 
-        public RepoViewHolder(View itemView) {
+        RepoViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
